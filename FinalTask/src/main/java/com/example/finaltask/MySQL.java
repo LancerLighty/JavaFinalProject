93d5f6a23a9cd4623c93c0f93b4944db77ff91e3
TekLighty <teklamamphoria4@gmail.com>
2022-09-15 22:00:11 +0300

    made changes in loginPage and MySQL

--- FinalTask/src/main/java/com/example/finaltask/MySQL.java
+++ FinalTask/src/main/java/com/example/finaltask/MySQL.java
@@ -2,7 +2,6 @@ package com.example.finaltask;
 
 import javax.servlet.annotation.WebServlet;
 import java.sql.*;
-import java.util.ArrayList;
 
 @WebServlet(name = "MySQL", value = "/MySQL")
 public class MySQL {
@@ -30,6 +29,7 @@ public class MySQL {
         }
         return info;
     }
+
     public void insertInfo(String a, String b, String c, String d, String e, String f, String g) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         try{
